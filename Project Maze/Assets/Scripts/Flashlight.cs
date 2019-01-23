f617ac6558cf1dd90fd0ed9f7dd2642f6e0b13a4
Amardeep Seeboruth <45082678+Sh4dez@users.noreply.github.com>
2019-01-23 15:39:33 -0500

    Flashlight included

---
+++ Project Maze/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Flashlight : MonoBehaviour
+{   //main flashlight
+    public Light flashlight;
+    //determine whether on or off
+    public bool triggerFlashlight;
+    //battery of the flashlight
+    public float flashlightBattery = 100f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight = GetComponent<Light>();
+        triggerFlashlight = false;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        flashlight.enabled = triggerFlashlight;
+        //press "g" to turn on or off 
+        if (Input.GetKeyDown("g"))
+        {
+            triggerFlashlight = !triggerFlashlight;
+        }
+
+        if (triggerFlashlight)
+        {
+            flashlightBattery -= Time.time * 0.0009f; // reduce battery by 0.01
+            if (flashlightBattery <= 0f)
+            {
+                triggerFlashlight = false;
+            }
+            
+            else if (flashlightBattery <= 20)
+            {
+                flashlight.intensity -= Time.time * 0.00001f;
+            }
+        }
+       
+    }
+}
