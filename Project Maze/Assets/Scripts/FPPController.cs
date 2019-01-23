851bb408a827806f753d8bee908e1fe1af0b0def
Rhainel <rhainel@users.noreply.github.com>
2019-01-23 00:34:56 -0500

    added first person controller

--- Project Maze/Assets/Scripts/FPPController.cs
+++ Project Maze/Assets/Scripts/FPPController.cs
@@ -2,17 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class FPPController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // public vars
+    public float mouseSensitivityX = 3.5f;
+    public float mouseSensitivityY = 3.5f;
+    public float walkSpeed = 6;
+    public float jumpForce = 220;
+    public float fallMultiplier = 2.5f;
+    
+    public LayerMask groundedMask;
+
+    // System vars
+    public bool grounded;
+    Vector3 moveAmount;
+    Vector3 smoothMoveVelocity;
+    float verticalLookRotation;
+    Transform cameraTransform;
+    private new Rigidbody rigidbody;
+
+
+    void Awake()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraTransform = Camera.main.transform;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        // Look rotation:
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
+        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -45, 45);
+        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
+
+        // Calculate movement:
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+        Vector3 targetMoveAmount = moveDir * walkSpeed;
+        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                rigidbody.AddForce(transform.up * jumpForce);
+            }
+        }
+        if (!grounded)
+        {
+            rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+        // Grounded check
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1.5f, groundedMask))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false; // todo set to false
+        }
+
+
+        // get back mouse cursor
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement to rigidbody
+        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+        rigidbody.MovePosition(rigidbody.position + localMove);
     }
 }
