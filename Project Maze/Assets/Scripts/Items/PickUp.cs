09bf89b4c53e47bf7594919eea16ea5480c64062
Rhainel <rhainel@users.noreply.github.com>
2019-01-23 22:07:41 -0500

    added player to level1

--- Project Maze/Assets/Scripts/Items/PickUp.cs
+++ Project Maze/Assets/Scripts/Items/PickUp.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections;
+﻿/*
+ Author: Rhainel Peralta
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 public class PickUp : MonoBehaviour
 {
